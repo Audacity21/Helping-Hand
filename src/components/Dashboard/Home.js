80a13e617b940b89da3532b08b1a7ce3bbda7ef1
Ankit Seth <iam4nkit@gmail.com>
2022-10-29 00:51:27 +0530

    made changes to dashboard

--- src/components/Dashboard/Home.js
+++ src/components/Dashboard/Home.js
@@ -3,7 +3,10 @@ import './Home.css';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { products } from '../../constants/Products';
+import { featuredProducts } from '../../constants/Products';
 
 function Home() {
   return (
@@ -22,6 +25,38 @@ function Home() {
       </div>
       <div className='dash-product-section'>
         <h3>Featured Product</h3>
+        <div className='featured-product'>
+          {featuredProducts.map((featured) => {
+            return(
+              <>
+                <div className='fp-image'>
+                  <img src={featured.image} alt={featured.id} />
+                </div>
+                <div className='fp-text'>
+                  <h1>{featured.title}</h1>
+                  <h5>Sold by {featured.seller}</h5>
+                  <h4>Rs. {featured.price}</h4>
+                  <MoreVertIcon className='fp-moreverticon' />
+                </div>
+              </>
+            )
+          })}
+        </div>
+        <h3>Products</h3>
+        <div className='products-list'>
+          {products.map((product) => {
+            return (
+              <div className='product-card'>
+                <img src={product.image} alt={product.id} />
+                <h3>{product.title}</h3>
+                <div className='product-footer'>
+                  <h5>By {product.seller}</h5>
+                  <h5>Rs. {product.price}</h5>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   )
